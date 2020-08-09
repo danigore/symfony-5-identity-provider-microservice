8b22372677aad6364e4a1df173c11791354dd63a
danigore <danigoredev@gmail.com>
2020-08-09 21:51:35 +0200

    Completed: After a logout request, a token refresh request is denied.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use App\Service\DependencyInjection\JWTCookieExtractorDependencyInjectionService
 use App\Exception\LogoutNotSupportedWithAuthorizationHeaderTypeTokenExtractorException;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Yaml\Exception\ParseException;
@@ -18,12 +19,13 @@ class SecurityController
 {
     /**
      * @Route("/logout", name="app_logout", methods={"GET"})
+     * @param Request $request
      * @param JWTCookieExtractorDependencyInjectionService $service
      * @return JsonResponse
      * @throws ParseException
      * @throws LogoutNotSupportedWithAuthorizationHeaderTypeTokenExtractorException
      */
-    public function logout(JWTCookieExtractorDependencyInjectionService $service): JsonResponse
+    public function logout(Request $request, JWTCookieExtractorDependencyInjectionService $service): JsonResponse
     {
         if ($service->isAuthorizationHeaderExtractorEnabled()) {
             throw new LogoutNotSupportedWithAuthorizationHeaderTypeTokenExtractorException('Backend logout not supported!');
@@ -31,6 +33,11 @@ class SecurityController
 
         $response = new JsonResponse(null, Response::HTTP_OK);
         $response->headers->setCookie(new Cookie($service->getCookieName(), '', 1));
+
+        if ($request->cookies->has($service->getRefreshCookieName())) {
+            $response->headers->setCookie(new Cookie($service->getRefreshCookieName(), '', 1));
+        }
+
         $response->send();
 
         return $response;
